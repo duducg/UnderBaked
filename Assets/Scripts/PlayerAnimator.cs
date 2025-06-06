569f69b0391c57c1466db26fe4315a4f5a89f31e
Eduardo Rocha <edu.sang.rocha@gmail.com>
2025-06-06 22:10:08 +0100

    Animator component synched with owner authorative

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour {
+public class PlayerAnimator : NetworkBehaviour {
 
 
     private const string IS_WALKING = "IsWalking";
@@ -19,6 +21,10 @@ public class PlayerAnimator : MonoBehaviour {
     }
 
     private void Update() {
+        if(!IsOwner)
+        {
+            return;
+        }
         animator.SetBool(IS_WALKING, player.IsWalking());
     }
 
