5466b023da434ee2a73194721d1c0f97cabcfbc0
Eduardo Rocha <edu.sang.rocha@gmail.com>
2025-06-07 19:18:30 +0100

    Added spawn position logic and player collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,7 +26,9 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private LayerMask countersLayerMask;
+    [SerializeField] private LayerMask collisionsLayerMask;
     [SerializeField] private Transform kitchenObjectHoldPoint;
+    [SerializeField] private List<Vector3> spawnPositionsList;
 
 
     private bool isWalking;
@@ -56,6 +58,8 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
         {
             LocalInstance = this;
         }
+        transform.position = spawnPositionsList[(int)OwnerClientId];
+        
         OnAnyPlayerSpawned?.Invoke(this, EventArgs.Empty);
     }
 
@@ -71,11 +75,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
         if(!IsOwner)
         {
             return; //If not owner (host) player then return
-        }
-        //Weird behavior with player speed. Disabling...
-        //Switch to Updating movement by sending the input to the server via RPC 
-        // And then the server moving the character in response.
-        //HandleMovementServerAuth();
+        }        
         HandleMovement();
         HandleInteractions();
     }
@@ -96,7 +96,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
         }
 
         float interactDistance = 2f;
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, collisionsLayerMask)) {
             if (raycastHit.transform.TryGetComponent(out BaseCounter baseCounter)) {
                 // Has ClearCounter
                 if (baseCounter != selectedCounter) {
@@ -110,64 +110,9 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
         } else {
             SetSelectedCounter(null);
         }
-    }
-    //Send requests to the server to move:
+    }   
 
-    //Runs on every Client, So all we want to capture is the input
-    private void HandleMovementServerAuth()
-    {
-        Vector2 inputVector =  GameInput.Instance.GetMovementVectorNormalized();
-        HandleMovementServerRPC(inputVector);
-    }
-
-    //Send the movement to the server via RPC
-    //Meaning the code bellow is going to run ONLY on the server side.
-    [ServerRpc(RequireOwnership = false)] //RPC's need to be tagged with this
-    private void HandleMovementServerRPC(Vector2 inputVector)
-    {
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        float moveDistance = moveSpeed * Time.deltaTime;
-        float playerRadius = .7f;
-        float playerHeight = 2f;
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
-
-        if (!canMove) {
-            // Cannot move towards moveDir
-
-            // Attempt only X movement
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = (moveDir.x < -.5f || moveDir.x > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
-
-            if (canMove) {
-                // Can move only on the X
-                moveDir = moveDirX;
-            } else {
-                // Cannot move only on the X
-
-                // Attempt only Z movement
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = (moveDir.z < -.5f || moveDir.z > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
-
-                if (canMove) {
-                    // Can move only on the Z
-                    moveDir = moveDirZ;
-                } else {
-                    // Cannot move in any direction
-                }
-            }
-        }
-
-        if (canMove) {
-            transform.position += moveDir * moveDistance;
-        }
-
-        isWalking = moveDir != Vector3.zero;
-
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-
-    }
+    
     private void HandleMovement() {
         Vector2 inputVector =  GameInput.Instance.GetMovementVectorNormalized();
 
@@ -176,14 +121,14 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = .7f;
         float playerHeight = 2f;
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        bool canMove = !Physics.BoxCast(transform.position,Vector3.one*playerRadius, moveDir,Quaternion.identity, moveDistance,collisionsLayerMask);
 
         if (!canMove) {
             // Cannot move towards moveDir
 
             // Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = (moveDir.x < -.5f || moveDir.x > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = (moveDir.x < -.5f || moveDir.x > +.5f) && !Physics.BoxCast(transform.position,Vector3.one*playerRadius, moveDirX,Quaternion.identity, moveDistance,collisionsLayerMask); 
 
             if (canMove) {
                 // Can move only on the X
@@ -193,7 +138,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
 
                 // Attempt only Z movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = (moveDir.z < -.5f || moveDir.z > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = (moveDir.z < -.5f || moveDir.z > +.5f) && !Physics.BoxCast(transform.position,Vector3.one*playerRadius, moveDirZ,Quaternion.identity, moveDistance,collisionsLayerMask); 
 
                 if (canMove) {
                     // Can move only on the Z
